ba0711877548abda1b1847548d9913bf808aa99e
Vanya <sinhavanya19@gmail.com>
2025-04-17 00:57:42 +0530

    Pipeline Folder

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,7 +43,7 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "Lasso Regression": Lasso(),
                 "Ridge Regression": Ridge(),
-                "SVR": SVR(),
+                "SVR": SVR(verbose=2),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Random Forest": RandomForestRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
@@ -53,10 +53,115 @@ class ModelTrainer:
                 "LightGBM": LGBMRegressor()
             }
 
+            params={
+                "Linear Regression":{},
+                "Lasso Regression": {
+                    'alpha': [0.0001, 0.001, 0.01, 0.05, 0.1, 1, 10, 50, 100],
+                    'fit_intercept': [True, False],
+                    'selection': ['cyclic', 'random'],
+                    'max_iter': [1000, 2000, 5000, 10000],
+                    'tol': [1e-4, 1e-3, 1e-2]
+                },
+                "Ridge Regression": {
+                    'alpha': [0.0001, 0.001, 0.01, 0.05, 0.1, 1, 10, 50, 100],
+                    'fit_intercept': [True, False],
+                    'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'],
+                    'max_iter': [1000, 2000, 5000, 10000],
+                    'tol': [1e-4, 1e-3, 1e-2]
+                    
+                },
+                "SVR": {
+                    
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                    'C': [0.01, 0.1, 1, 10],
+                    'epsilon': [0.001, 0.01, 0.05, 0.1],
+                    'gamma': ['scale', 'auto', 0.001, 0.01],
+                    #'degree': [2, 3, 4, 5],  # for 'poly' kernel
+                    #'shrinking': [True, False]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                    'max_depth': [None, 5, 10, 20, 30, 40, 50, 75, 100],  # limits tree depth
+                    'min_samples_split': [2, 5, 10, 15, 20, 50],  # min samples to split an internal node
+                    'min_samples_leaf': [1, 2, 4, 6, 10, 20],  # miEn samples at a leaf node
+                    'min_weight_fraction_leaf': [0.0, 0.01, 0.05, 0.1],  # min weighted fraction of total sum of weights at leaf
+                    'max_features': [None, 'sqrt', 'log2', 0.1, 0.3, 0.5, 0.7],  # number of features to consider for best split
+                    'max_leaf_nodes': [None, 10, 20, 30, 40, 50, 100],  # limits leaf nodes
+                    'min_impurity_decrease': [0.0, 0.01, 0.02, 0.05],  # early stopping criterion
+                    'ccp_alpha': [0.0, 0.001, 0.005, 0.01, 0.05],  # post-pruning parameter (cost-complexity pruning)
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['auto', 'sqrt', 'log2', 0.1, 0.2, 0.3, 0.5, None],
+                    'n_estimators': [8,16,32,64,128,256],
+                    'max_depth': [None, 5, 10, 20, 30, 40, 50, 75, 100],  # control overfitting
+                    'min_samples_split': [2, 5, 10, 15, 20, 50],  # minimum to split an internal node
+                    'min_samples_leaf': [1, 2, 4, 6, 10, 20],  # minimum samples at a leaf
+                    'max_leaf_nodes': [None, 10, 20, 30, 40, 50],  # limits leaf nodes
+                    'bootstrap': [True, False],  # whether bootstrap sampling is used
+                    'oob_score': [True, False],  # only when bootstrap=True
+                    'ccp_alpha': [0.0, 0.001, 0.005, 0.01, 0.05],  # complexity pruning
+                    'max_samples': [None, 0.3, 0.5, 0.7, 0.9],  # for subsampling in each tree (only if bootstrap=True)
+                    'min_weight_fraction_leaf': [0.0, 0.01, 0.05],  # rarely used but helps in imbalance cases
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001, 0.2, 0.3],
+                    'subsample':[0.5,0.6,0.7,0.75,0.8,0.85,0.9, 1.0],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256,100, 200, 300, 500, 800, 1000],
+                    'max_depth': [3, 5, 7, 10, 15, 20],
+                    'min_samples_split': [2, 5, 10, 15],
+                    'min_samples_leaf': [1, 2, 4, 8, 10],
+                    'max_features': ['auto', 'sqrt', 'log2', 0.2, 0.5, 0.7, 1.0],
+                },
+                "Ada Boost":{
+                    'learning_rate':[.1,.01,0.5,.001, 0.05, 1.0],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256,50, 100, 200, 300, 500, 1000]
+                                
+                },
+                "KNN": {
+                    'n_neighbors': [3, 5, 7, 9, 11, 13, 15, 20, 25, 30],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    'leaf_size': [10, 20, 30, 40, 50, 60],
+                    'p': [1, 2, 3]  # 1 = Manhattan, 2 = Euclidean, 3 = Minkowski
+                },
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001, 0.2],
+                    'n_estimators': [8,16,32,64,128,256,100, 200, 300, 500, 800],
+                    'max_depth': [3, 5, 7, 10, 15],
+                    'min_child_weight': [1, 3, 5, 7],
+                    'subsample': [0.5, 0.7, 0.8, 1.0],
+                    'colsample_bytree': [0.5, 0.7, 0.8, 1.0],
+                    'gamma': [0, 0.1, 0.3, 0.5, 1.0],
+                    'reg_alpha': [0, 0.01, 0.1, 1, 10],
+                    'reg_lambda': [0, 0.01, 0.1, 1, 10],
+                    'scale_pos_weight': [1, 2, 5],  # useful in imbalanced regression targets
+                    'booster': ['gbtree', 'gblinear', 'dart']
+                },
+                "LightGBM": {
+                    'n_estimators': [100, 200, 500, 800, 1000],
+                    'learning_rate': [0.001, 0.01, 0.05, 0.1, 0.2],
+                    'max_depth': [-1, 3, 5, 7, 10, 15, 20],
+                    'num_leaves': [20, 31, 40, 60, 80, 100],
+                    'min_child_samples': [5, 10, 20, 30, 50],
+                    'subsample': [0.5, 0.6, 0.8, 1.0],
+                    'colsample_bytree': [0.5, 0.6, 0.8, 1.0],
+                    'reg_alpha': [0.0, 0.01, 0.1, 0.5, 1.0],
+                    'reg_lambda': [0.0, 0.01, 0.1, 0.5, 1.0],
+                    'boosting_type': ['gbdt', 'dart', 'goss']
+                }   
+            }
+
             #Evaluate models will be present in utils
             logging.info("Evaluating of models started")
             model_report:dict=evaluate_model(x_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,params=params)
             
             # Get best model score from the dictionary
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import numpy as np
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 
 import dill
 
@@ -24,7 +25,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise Exception(f"Error saving object: {str(e)}")
     
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models,params):
     """
     Evaluate the model and return the best model based on R2 score.
     """
@@ -34,7 +35,17 @@ def evaluate_model(x_train, y_train, x_test, y_test, models):
         model_names = list(models.keys())
         for i in range(len(models)):
             model = model_values[i]
-            model.fit(x_train, y_train)
+            para=params[model_names[i]]
+            if model_names[i]=='SVR':
+                rs=RandomizedSearchCV(model,para,cv=3,n_jobs=-1,n_iter=30)
+            else:
+                rs=RandomizedSearchCV(model,para,cv=3,n_jobs=-1)
+
+            rs.fit(x_train,y_train)
+            logging.info(f"{model} best params are : {rs.best_params_} and best score is: {rs.best_score_}")
+            model.set_params(**rs.best_params_)
+            model.fit(x_train,y_train)
+            
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -43,3 +54,12 @@ def evaluate_model(x_train, y_train, x_test, y_test, models):
         return report
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+    
